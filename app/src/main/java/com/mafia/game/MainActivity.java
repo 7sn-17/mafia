efbe16ff5f0518815445a45777a8ba534f0ebfdb
7sn <hasantaha5689@gmail.com>
2026-09-18 23:49:35 +0300

    Update MainActivity.java

--- app/src/main/java/com/mafia/game/MainActivity.java
+++ app/src/main/java/com/mafia/game/MainActivity.java
@@ -2,6 +2,9 @@ package com.mafia.game;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -9,17 +12,23 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
+        // جعل التطبيق يمتد على كامل الشاشة (Edge-to-Edge)
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        );
+
         WebView webView = new WebView(this);
         WebSettings settings = webView.getSettings();
         
-        // تفعيل الجافاسكربت وقواعد البيانات لتعمل اللعبة (Firebase)
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
-        
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+
         setContentView(webView);
-        
-        // تشغيل ملف الويب الخاص بك
         webView.loadUrl("file:///android_asset/index.html");
     }
 }
